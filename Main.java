be4aba69e26ae5ad51b5c6a8d0132611b666dc97
JustinEspedido <espedidojustin12@gmail.com>
2025-02-23 23:44:48 +0800

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,28 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    Person myPerson = new Person();
+        
+        myPerson.a1 = "Celestis";
+    	myPerson.a2 = "Willowgrove";
+        myPerson.a3 = 21;
+    	    
+        myPerson.b1 = "Xaviera";
+        myPerson.b2 = "Solus";
+        myPerson.b3 = 21;
+    	    
+        myPerson.c1 = "Charlotte";
+        myPerson.c2 = "Aquaria";
+        myPerson.c3 = 21;
+    	    
+        myPerson.d1 = "Lily";
+        myPerson.d2 = "Fellfield";
+        myPerson.d3 = 21;
+    	    
+        myPerson.e1 = "Melissa";
+        myPerson.e2 = "Everdawn";
+        myPerson.e3 = 21;
+        
+        myPerson.display();
+	}
+}
